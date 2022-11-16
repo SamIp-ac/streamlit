c66b534a4a20ca71f697eca483ec56b92654d90c
SamIp-ac <i60996395@gmail.com>
2022-11-16 14:07:28 +0800

    change a word

--- app/st2.py
+++ app/st2.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 2000, 2000)
+    total_points = st.slider("Number of points in spiral", 1, 2500, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
