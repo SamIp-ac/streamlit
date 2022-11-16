df2f172f351f082c2842bd5d49980e80cbfb0fa1
SamIp-ac <i60996395@gmail.com>
2022-11-16 15:16:24 +0800

    change file position

--- pages/page_2.py.py
+++ pages/page_2.py.py
@@ -4,19 +4,11 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 3000, 1000)
+    total_points = st.slider("Number of points in spiral", 1, 2500, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
