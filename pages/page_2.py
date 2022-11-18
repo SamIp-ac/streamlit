771504644f7f6000ced150d787ad0ba90b846787
SamIp-ac <i60996395@gmail.com>
2022-11-18 13:28:21 +0800

    add and change name

--- pages/page_2.py
+++ pages/page_2.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+# defines an h1 header
+st.title("Rhythm detection")
+
+# displays a file uploader widget
+audio_file = st.file_uploader("Upload a audio (.wav)")
+
+if st.button("Style Transfer"):
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes)
