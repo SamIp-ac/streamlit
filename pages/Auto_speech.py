dbdb2e9f5af216cb5f0e2dbeeca5693dc94a79e0
samip-ac <i60996395@gmail.com>
2022-12-12 13:47:23 +0800

    add and change name

--- pages/Auto_speech.py
+++ pages/Auto_speech.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown("# Transform mxl file to speech")
+st.markdown("# Transform mxl file to speech (Not finished yet)")
 st.sidebar.markdown("# mxl to wav speech convertor")
-
 
 mxlfile = st.file_uploader("Upload a mxl file")
