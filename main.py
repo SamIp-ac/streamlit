8eab522de1408db02effcaeadfbef91ce86ec173
samip-ac <i60996395@gmail.com>
2023-01-11 10:21:17 +0800

    updated README.md

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.markdown("# Music application lab -- web app ")
 st.sidebar.markdown("# different experimental project ")
 
-st.write('### Including Rhythm detection, auto-speech, mxl to wav, mxl to unicode \n'
+st.write('### Including audio to list, rhythm detection, auto-speech, mxl to wav, mxl to unicode \n'
          '### Click the left bar and try it.')
 
